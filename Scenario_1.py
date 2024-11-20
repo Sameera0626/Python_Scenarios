9e108efc6e1aff551f53af8e9952f3802e7a1ac2
Sameera Banu <sameerabanu0626@gmail.com>
2024-11-20 21:28:29 +0530

    Update Scenario_1.py

--- Scenario_1.py
+++ Scenario_1.py
@@ -1,1 +1,17 @@
+Task:
+And if you have dictionay in which key will be playersname and value will the runs they score ..write the python code to write and fetch the maximum runs players scores??
 
+#sample dictionary of players and their runs
+players_score={
+  "Kohli" : 85,
+  "Sachin": 72,
+  "Rohit Sharma" : 99,
+  "Rahul" : 45,
+  "hardik" : 60
+}
+
+#get the maximun run player scored
+max_player=max(players_score,key=players_score)
+max_score=players_score[max_player]
+
+print(f"the player with maximum run is {max_player} with{max_score} runs")
